409472f88a14e2c6d941d1226d80b9f746c07a99
amogus19k <amogus19kpuk@gmail.com>
2026-07-13 21:17:23 +0300

    Update hero heading and add GitHub link to repo

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Говорим Next.js сгенерировать статические файлы (HTML/CSS/JS)
+  basePath: '/axiom-war-site', // Указываем имя твоего репозитория на GitHub
   typescript: {
     ignoreBuildErrors: true,
   },
